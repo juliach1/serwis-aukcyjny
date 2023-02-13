925fc220f3f8265c9c6e153d2b3883189b9d7761
Julia <julia.00.chmielewska@gmail.com>
2023-02-13 11:53:43 +0100

    Dodanie projektu do repozytorium

--- aukcje/src/main/java/com/aukcje/controller/dictionaries/MainPageController.java
+++ aukcje/src/main/java/com/aukcje/controller/dictionaries/MainPageController.java
@@ -17,7 +17,6 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
-
 import javax.validation.Valid;
 import java.util.List;
 

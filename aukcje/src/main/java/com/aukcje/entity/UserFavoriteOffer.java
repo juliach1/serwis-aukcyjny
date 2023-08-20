4fa3055d79b8516479e659b18a4f81c149e3c618
juliach1 <julia.00.chmielewska@gmail.com>
2023-08-20 18:35:20 +0200

    Utowrzono encję UserFavoriteOffer

---
+++ aukcje/src/main/java/com/aukcje/entity/UserFavoriteOffer.java
@@ -0,0 +1,32 @@
+package com.aukcje.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user-favorite-offer")
+@Setter
+@Getter
+@NoArgsConstructor
+public class UserFavoriteOffer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
+    @JoinColumn(name="ID_USER")
+    private User user;
+
+    @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
+    @JoinColumn(name="ID_OFFER")
+    private Offer offer;
+
+    @JoinColumn(name="INSERT_TIME")
+    private LocalDateTime insertTime;
+}

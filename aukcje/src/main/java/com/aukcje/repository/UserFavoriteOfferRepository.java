2c722e41f88b97808f207bd286ae35d1c6684a49
juliach1 <julia.00.chmielewska@gmail.com>
2023-08-20 18:44:06 +0200

    Utowrzono repozytorium dla ulubionych ofert

---
+++ aukcje/src/main/java/com/aukcje/repository/UserFavoriteOfferRepository.java
@@ -0,0 +1,7 @@
+package com.aukcje.repository;
+import com.aukcje.entity.UserFavoriteOffer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserFavoriteOfferRepository extends JpaRepository<UserFavoriteOffer, Long> {
+
+}
